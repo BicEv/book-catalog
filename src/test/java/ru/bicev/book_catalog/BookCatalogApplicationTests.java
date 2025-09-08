496a5680fb0fe9c9aa09f71dec8bd9b73012b0a4
BicEv <bickbovev@gmail.com>
2025-09-08 16:59:27 +0300

    project structure

---
+++ src/test/java/ru/bicev/book_catalog/BookCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bicev.book_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
