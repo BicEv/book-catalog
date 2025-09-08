c2c6bf5d1ae3bdc30bbf9d9068c5f73ff30a701e
BicEv <bickbovev@gmail.com>
2025-09-08 19:45:57 +0300

    Added search by author name part method

--- src/main/java/ru/bicev/book_catalog/repo/BookRepository.java
+++ src/main/java/ru/bicev/book_catalog/repo/BookRepository.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import ru.bicev.book_catalog.entity.Book;
 import ru.bicev.book_catalog.util.Genre;
@@ -12,6 +13,15 @@ import ru.bicev.book_catalog.util.Genre;
 public interface BookRepository extends JpaRepository<Book, UUID> {
 
     Page<Book> findByAuthorId(UUID authorId, Pageable pageable);
+
+    @Query("""
+            SELECT b
+            FROM Book b
+            JOIN b.author a
+            WHERE LOWER(CONCAT(a.firstName,' ',a.lastName))
+            LIKE LOWER(CONCAT('%',:name,'%'))
+            """)
+    Page<Book> findByAuthorName(String name, Pageable pageable);
 
     Page<Book> findByReleaseYear(int releaseYear, Pageable pageable);
 
