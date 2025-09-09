be82b0792049b8f05ed0fbe4611364bc802d7ef0
BicEv <bickbovev@gmail.com>
2025-09-09 19:57:43 +0300

    BookRestController

---
+++ src/main/java/ru/bicev/book_catalog/controller/BookRestController.java
@@ -0,0 +1,91 @@
+package ru.bicev.book_catalog.controller;
+
+import java.net.URI;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import ru.bicev.book_catalog.dto.BookDto;
+import ru.bicev.book_catalog.dto.BookRequest;
+import ru.bicev.book_catalog.service.BookService;
+import ru.bicev.book_catalog.util.Genre;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookRestController {
+
+    private final BookService bookService;
+
+    public BookRestController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public ResponseEntity<BookDto> createBook(@Valid @RequestBody BookRequest bookRequest) {
+        BookDto created = bookService.createBook(bookRequest);
+        URI location = URI.create("/api/books/" + created.id().toString());
+        return ResponseEntity.created(location).body(created);
+    }
+
+    @GetMapping("/{bookId}")
+    public ResponseEntity<BookDto> getBookById(@PathVariable UUID bookId) {
+        BookDto book = bookService.findBookById(bookId);
+        return ResponseEntity.ok().body(book);
+    }
+
+    @PutMapping("/{bookId}")
+    public ResponseEntity<BookDto> updateBook(@PathVariable UUID bookId, @Valid @RequestBody BookRequest bookRequest) {
+        BookDto updated = bookService.updateBook(bookId, bookRequest);
+        return ResponseEntity.ok().body(updated);
+    }
+
+    @DeleteMapping("/{bookId}")
+    public ResponseEntity<Void> deleteBook(@PathVariable UUID bookId) {
+        bookService.deleteBook(bookId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<BookDto>> getBooks(
+            @RequestParam(required = false) UUID authorId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) Integer releaseYear,
+            @RequestParam(required = false) Integer startYear,
+            @RequestParam(required = false) Integer endYear,
+            @RequestParam(required = false) Genre genre,
+            @RequestParam(required = false) String title,
+            @PageableDefault(size = 10, sort = "title") Pageable pageable) {
+        Page<BookDto> page;
+        if (authorId != null) {
+            page = bookService.findByAuthorId(authorId, pageable);
+        } else if (name != null) {
+            page = bookService.findByAuthorName(name, pageable);
+        } else if (releaseYear != null) {
+            page = bookService.findByReleaseYear(releaseYear, pageable);
+        } else if (startYear != null && endYear != null && startYear <= endYear) {
+            page = bookService.findByReleaseYearBetween(startYear, endYear, pageable);
+        } else if (genre != null) {
+            page = bookService.findByGenre(genre, pageable);
+        } else if (title != null) {
+            page = bookService.findByTitleContaining(title, pageable);
+        } else {
+            page = bookService.findAll(pageable);
+        }
+
+        return ResponseEntity.ok(page);
+    }
+
+}
