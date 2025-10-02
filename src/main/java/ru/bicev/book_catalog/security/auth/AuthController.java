5a405ba59d4133f5b110e25b9121105d892fd97f
BicEv <bickbovev@gmail.com>
2025-10-02 18:11:19 +0300

    AuthController

---
+++ src/main/java/ru/bicev/book_catalog/security/auth/AuthController.java
@@ -0,0 +1,39 @@
+package ru.bicev.book_catalog.security.auth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ru.bicev.book_catalog.security.dto.UserRequest;
+import ru.bicev.book_catalog.security.jwt.JwtUtil;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody UserRequest userRequest) {
+        try {
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(userRequest.username(), userRequest.password()));
+            String token = jwtUtil.generateToken(userRequest.username());
+            return ResponseEntity.ok(token);
+        } catch (Exception e) {
+            return ResponseEntity.status(401).body("Invalid credentials");
+        }
+    }
+
+}
